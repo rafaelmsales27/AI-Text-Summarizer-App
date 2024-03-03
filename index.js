c8409058c5aca5edbc9cfe454eb2f3ef4db05d64
Rafael Sales <rafaelmsales27@gmail.com>
2024-03-03 20:19:16 +0100

    update summarize.js call

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const summarizeText = require('./functions/summarize.js');
+const summarizeText = require('./summarize.js');
 const app = express();
 const port = 3000;
-
-
-console.log('ACCESS_TOKEN:', process.env.ACCESS_TOKEN);
 
 // Parses JSON bodies (as sent by API clients)
 app.use(express.json());
